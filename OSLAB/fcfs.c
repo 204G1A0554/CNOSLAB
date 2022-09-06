ba7cdfad87b34f13edf2fe0a025779910e819107
c-manojreddy <94514858+c-manojreddy@users.noreply.github.com>
2022-09-06 12:37:41 +0530

    Update fcfs.c

--- OSLAB/fcfs.c
+++ OSLAB/fcfs.c
@@ -1,3 +1,9 @@
+/* Name Of the Candidate: C.MANOJREDDY
+   Roll No: 204G1A0554
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
 #include<stdio.h>
 main()
 {
